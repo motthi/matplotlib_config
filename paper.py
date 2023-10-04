ac677201f2f3cdebbc5b5b5b5916dfc49b1691de
motthi <45708319+pasises@users.noreply.github.com>
2023-10-04 23:48:24 +0900

    first commit

---
+++ paper.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.family'] = 'Arial'
+plt.rcParams['figure.dpi'] = 300
+plt.rcParams['mathtext.fontset'] = 'stix'
+plt.rcParams["font.size"] = 15              # Font size for all text (titles, labels, ticks, text, etc.)
+plt.rcParams['xtick.labelsize'] = 11        # Label size for x axis ticks
+plt.rcParams['ytick.labelsize'] = 11        # Label size for y axis ticks
+plt.rcParams['xtick.direction'] = 'in'      # x axis in
+plt.rcParams['ytick.direction'] = 'in'      # y axis in
+plt.rcParams['axes.linewidth'] = 1.0        # axis line width
+plt.rcParams['axes.grid'] = True
+plt.rcParams["legend.fancybox"] = False     # Rounded corners for legend box
+plt.rcParams["legend.framealpha"] = 1
+plt.rcParams["legend.edgecolor"] = 'white'
+plt.rcParams["legend.fontsize"] = 10
+plt.rcParams["legend.handlelength"] = 3     # The length of the legend handles
+plt.rcParams["legend.labelspacing"] = 1.    # The vertical space between the legend entries
+plt.rcParams["legend.handletextpad"] = 1.   # The pad between the legend handle and text
+plt.rcParams["legend.markerscale"] = 2      # The relative size of legend markers vs. original
+plt.rcParams["legend.borderaxespad"] = 0.1  # The border between the axes and legend edge
+plt.rcParams["savefig.bbox"] = 'tight'
+plt.rcParams["savefig.pad_inches"] = 0.1
+plt.rcParams["savefig.dpi"] = 300
